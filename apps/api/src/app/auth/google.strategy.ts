e1c5400f3ef747bd6e96a916f24a43d386b3bc27
Enqutk <enkukokob@gmail.com>
2026-05-20 14:17:35 +0300

    feat: update GoogleStrategy to use dynamic callback URL from config

--- apps/api/src/app/auth/google.strategy.ts
+++ apps/api/src/app/auth/google.strategy.ts
@@ -13,7 +13,9 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:3000/api/auth/google/callback',
+      callbackURL:
+        configService.get<string>('GOOGLE_CALLBACK_URL') ||
+        'http://localhost:3000/api/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }

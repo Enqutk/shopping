dfb1f656e34a880011683d95589f8e53a7ec49a1
Enqutk <enkukokob@gmail.com>
2026-05-21 16:02:04 +0300

    feat: export additional modules for categories and admin DTOs in shared index

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,5 +1,7 @@
 export * from './lib/shared.js';
+export * from './lib/categories.js';
 export * from './lib/dto/user.js';
 export * from './lib/dto/auth.js';
 export * from './lib/dto/product.js';
 export * from './lib/dto/order.js';
+export * from './lib/dto/admin.js';
